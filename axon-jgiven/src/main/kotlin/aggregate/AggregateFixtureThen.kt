d6b9a1b94ebb561ad3a3771369d00931d003f0f5
Jan Galinski <jan.galinski@holisticon.de>
2019-02-15 15:09:08 +0100

    then supports exception message

--- axon-jgiven/src/main/kotlin/aggregate/AggregateFixtureThen.kt
+++ axon-jgiven/src/main/kotlin/aggregate/AggregateFixtureThen.kt
@@ -4,6 +4,7 @@ import com.tngtech.jgiven.Stage
 import com.tngtech.jgiven.annotation.*
 import io.toolisticon.addons.axon.jgiven.AxonStage
 import org.axonframework.test.aggregate.ResultValidator
+import org.hamcrest.Matcher
 import org.hamcrest.MatcherAssert
 
 
@@ -17,22 +18,38 @@ class AggregateFixtureThen<T> : Stage<AggregateFixtureThen<T>>() {
   fun expectEvent(@Quoted event: Any) = this.expectEvents(event)
 
   @As("expect events:")
-  fun expectEvents(@Quoted @Table vararg events: Any) = resultValidator { resultValidator.expectEvents(*events) }
+  fun expectEvents(@Quoted @Table vararg events: Any) = execute { resultValidator.expectEvents(*events) }
 
   @As("expect state: $=$")
-  fun <E : Any> expectState(@Quoted field:String, @Quoted expected: E, @Hidden accessor: (T) -> E ) = self().apply {
+  fun <E : Any> expectState(@Quoted field: String, @Quoted expected: E, @Hidden accessor: (T) -> E) = execute {
     resultValidator.expectState {
       val e = accessor(it)
       MatcherAssert.assertThat("state failed, expected $field=$expected, but was=$e", e == expected)
-     // assert( e == expected ) { "expected state: '$name' not met, was: $e, expected: $expected " } }
-  }}!!
+      // assert( e == expected ) { "expected state: '$name' not met, was: $e, expected: $expected " } }
+    }
+  }
 
   @As("expect state: $")
-  fun expectState(@Quoted expected: T) = self().apply {
+  fun expectState(@Quoted expected: T) = execute {
     resultValidator.expectState {
       MatcherAssert.assertThat("state failed, expected '$expected', but was=$it", it == expected)
     }
-  }!!
+  }
 
-  private fun resultValidator(block : () -> ResultValidator<T>) = self().apply { resultValidator = block.invoke() }!!
+  @As("expect exception message: $")
+  fun expectExceptionMessage(@Quoted exceptionMessage: String) = execute {
+    resultValidator.expectExceptionMessage(exceptionMessage)
+  }
+
+  @As("expect matching exception message")
+  fun expectMatchingExceptionMessage(matcher: Matcher<String>) = execute {
+    resultValidator.expectExceptionMessage(matcher)
+  }
+
+  @As("expect successful handler execution")
+  fun expectSuccessfulHandlerExecution() = execute {
+    resultValidator.expectSuccessfulHandlerExecution()
+  }
+
+  private fun execute(block: () -> ResultValidator<T>) = self().apply { resultValidator = block.invoke() }!!
 }
