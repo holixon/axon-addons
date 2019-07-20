09930d71118f2c10b7f9431e572c03a444ffc071
Jan Galinski <jan.galinski@holisticon.de>
2019-07-21 01:07:14 +0200

    remove commented

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,19 +18,6 @@ allprojects {
   }
 }
 
-//
-//dependencies {
-//    // Use the Kotlin JDK 8 standard library.
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//
-//    // Use the Kotlin test library.
-//    testImplementation("org.jetbrains.kotlin:kotlin-test")
-//
-//    // Use the Kotlin JUnit integration.
-//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-//}
-
-
 dependencies {
   // Make the root project archives configuration depend on every sub-project
   subprojects.forEach {
