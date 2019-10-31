6c37429cdc8b11f865070befc71e41c07d28fb43
Jan Galinski <jan.galinski@holisticon.de>
2019-10-31 15:39:58 +0100

    update axon version to 4.2, fixes #22

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,13 @@
+import org.gradle.api.JavaVersion
 import org.gradle.kotlin.dsl.embeddedKotlinVersion
 
 object Versions {
 
   val kotlin = embeddedKotlinVersion
-  val java = "1.8"
-  val axon = "4.1.2"
+  val java = JavaVersion.VERSION_1_8.toString()
+  val axon = "4.2"
+
+  val springBoot = "2.2.0.RELEASE"
 
   val jgivenAddons = "0.5.1"
 
@@ -14,10 +17,17 @@ object Versions {
     val bintray = "1.8.4"
   }
 
+  object Test {
+    val jgiven = "0.18.2"
+    val junit5 = "5.5.2"
+  }
+
 
   val mapdb = "3.0.7"
   val klogging = "1.6.26"
 
+  @Deprecated("use Test.jgiven")
   val jgiven = "0.17.1"
+  @Deprecated("use Test.junit5")
   val junit5 = "5.4.2"
 }
